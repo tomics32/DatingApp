7e47ca2a96b4e94a7ccf9892dd0fd15b3b7a27d1
Tomasz Kalicki <tomics32@gmail.com>
2024-03-20 12:45:09 +0100

    -Its now possible to cancel registration form -Registration form now shows the list of all registered users (Just for testing)

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,15 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
+  users: any;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
-
+    this.getUsers();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode
+  }
+
+  getUsers()
+  {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    })
+  }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode=event;
   }
 
 }
