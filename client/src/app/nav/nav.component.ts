b7e6f83ee26892199e6b7670afe80b2575fc0837
Tomasz Kalicki <tomics32@gmail.com>
2024-03-23 14:21:14 +0100

    Routing added to project: -All links at the navbar are working fine with routing -User gets redirect to "members" template after logging in -User gets redirect to "HomePage" after logging out

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
-import { User } from '../_models/user';
-import { AppComponent } from '../app.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -16,17 +15,16 @@ export class NavComponent implements OnInit {
 
   constructor(
     public accountService: AccountService,
-    ) { }
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
-  
+
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
+      next: _ => this.router.navigateByUrl('/members'),
       error: error => console.log(error)
     })
 
@@ -34,6 +32,7 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
