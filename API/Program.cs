b8cbbed12728edad01898888f6a1e91f212f39c3
Tomasz Kalicki <tomics32@gmail.com>
2024-03-26 12:25:57 +0100

    -Fundamentals of error handling in .NET

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,5 @@
-using System.Text;
-using API.Data;
 using API.Extensions;
-using API.interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +11,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")); //dotnet watch --no-hot-reload
