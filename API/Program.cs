e6244911d08fb11b4aa5b0a6ae67b42a31b44341
Tomasz Kalicki <tomics32@gmail.com>
2024-05-20 15:24:31 +0200

    -Added middleware to handle error exceptions

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,7 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")); //dotnet watch --no-hot-reload
 
 app.UseAuthentication();
