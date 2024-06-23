a3e7f73cf471c5e5e6cf658f752d143b9bd9fb99
Tomasz Kalicki <tomics32@gmail.com>
2024-06-23 16:38:51 +0200

    Members are now displayed as member cards.

---
+++ client/src/app/members/member-card/member-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+
+@Component({
+  selector: 'app-member-card',
+  templateUrl: './member-card.component.html',
+  styleUrls: ['./member-card.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class MemberCardComponent {
+  @Input () member: Member | undefined;
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+}
