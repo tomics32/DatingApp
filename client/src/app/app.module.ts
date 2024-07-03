009f1609d639c538bd800788cdf24380449a77e0
Tomasz Kalicki <tomics32@gmail.com>
2024-07-03 15:07:36 +0200

    Register form has been changed to reactive form with validation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
@@ -46,7 +46,8 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ],
   providers: [NavComponent,
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor,multi: true},
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { NavComponent } from '../nav/nav.component';
 import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,6 +12,7 @@ import { ToastrService } from 'ngx-toastr';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter()
   model: any = {}
+  registerForm: FormGroup = new FormGroup(({}));
 
   constructor(
     private accountService: AccountService,
@@ -18,11 +20,29 @@ export class RegisterComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
 
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(16)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn{
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true}
+    }
   }
 
  register() {
-  this.accountService.register(this.model).subscribe({
+  console.log(this.registerForm?.value)
+ /* this.accountService.register(this.model).subscribe({
     next: () => {
       this.cancel();
     },
@@ -30,7 +50,7 @@ export class RegisterComponent implements OnInit {
       this.toastr.error(error.error);
       console.log(error);
     }
-  })
+  }) */
  }
 
  cancel(){
