9029171462f839ad9708809e088a19a04eed04ac
Tomasz Kalicki <tomics32@gmail.com>
2025-02-06 13:14:05 +0100

    -Angular application can now communicate with .NET backend Web API. Tested by displaying list of users stored in backend database -Added CORS to be able to communicate with backend -Added bootstrap and font awesome to project

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +10,21 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
   title = 'DatingApp';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe(
+      {
+        next: response => {
+          this.users = response,
+          console.log(this.users)
+        },
+        error: error => console.log(error),
+        complete: () => console.log('Response has completed')
+      }
+    )
+  }
 }
