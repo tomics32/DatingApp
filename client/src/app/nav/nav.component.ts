e37625c8b60eabb9668f4dd166e9482f371ff6a1
Tomasz Kalicki <tomics32@gmail.com>
2025-02-17 12:31:16 +0100

    -Navigation bar tabs are now only visible for logged in users -Dropdown menu is now visible for logged in users instead of login form -User information are now stored in local storage of a browser to persist login session

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,27 +1,30 @@
-import { Component, inject, Inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AccountService } from '../_services/account.service';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-  private accountService = inject(AccountService);
-  loggedIn = false;
+  accountService = inject(AccountService);
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
-        console.log(response),
-          this.loggedIn = true
+        console.log(response);
       },
       error: error => console.log(error)
     })
+  }
+
+  logout() {
+    this.accountService.logout();
   }
 }
 
