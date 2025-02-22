ceac7f42f39d90338d0fc480c79d6fbfb11be2d2
Tomasz Kalicki <tomics32@gmail.com>
2025-02-22 13:48:06 +0100

    -Toastr service has has been added to the project to display animated  errors on screen -Added route guard to prevent unauthorized users from accessing some tabs -Navbar now has more suitable colors for dating application

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -9,6 +10,9 @@ export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
      provideRouter(routes),
      provideHttpClient(),
-     provideAnimations()
+     provideAnimations(),
+     provideToastr({
+      positionClass: 'toast-bottom-right'
+     })
     ],
 };
