76292fe3f0576a7f2b171385f2e8f055580c58c7
Tomasz Kalicki <tomics32@gmail.com>
2025-03-15 16:42:28 +0100

    -Loading indicator has been added to the site. Its engaged when application is making api calls to backend -Optimalization of application has been increased by persisting states by storing members in signal instead of calling api each time

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -11,19 +11,15 @@ import { MemberCardComponent } from "../member-card/member-card.component";
   styleUrl: './member-list.component.css'
 })
 export class MemberListComponent implements OnInit {
-  private memberService = inject(MembersService);
-  members: Member[] = []
+  memberService = inject(MembersService);
 
   ngOnInit(): void {
-    this.loadMembers();
+    if (this.memberService.members().length === 0){
+      this.loadMembers();
+    }
   }
 
   loadMembers() {
-    this.memberService.getMembers().subscribe({
-      next: response => {
-        this.members = response
-      },
-      error: error => console.log(error)
-    })
+    this.memberService.getMembers();
   }
 }
